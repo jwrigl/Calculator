d5da4a7f6ffc76447b9be29166650af31e2a9289
jwrigl <124375480+jwrigl@users.noreply.github.com>
2023-04-29 15:56:55 +0100

    Create functions to build calculator buttons and output screen. Create function for button listener

--- script.js
+++ script.js
@@ -11,5 +11,71 @@ function multiply(numberOne, numberTwo) {
 }
 
 function divide(numberOne, numberTwo) {
-    return numberOne / numberTwo;
+    if (numberTwo === 0) {
+        console.log("Error 404, Chuck Norris not found.");
+    }
+    else {
+        return numberOne / numberTwo;
+    }
+}  
+
+function buildCalculator() {
+    buildOutputScreen()
+    buildNumberGrid()
+    buildOperatorGrid()
 }
+
+function buildNumberGrid() {
+    let container = document.querySelector("#numberContainer");
+    for (i=9;i>=0;i--) {
+        let btn = document.createElement("button");
+        btn.setAttribute("id",i);
+        btn.setAttribute("class","numberButtons");
+        btn.classList.add("calculatorButtons")
+        btn.innerText=i
+        container.appendChild(btn)
+    }
+    container.style.display = "grid";
+    container.style.gridTemplateColumns=`repeat(3), 20px)`;
+
+}
+
+function buildOperatorGrid() {
+    let container = document.querySelector("#operatorContainer");
+    let operators = ["*","+","-","/"];
+    for (i=0;i<4;i++) {
+        let btn = document.createElement("button");
+        btn.setAttribute("id",operators[i]);
+        btn.setAttribute("class","operatorButtons");
+        btn.classList.add("calculatorButtons")
+        btn.innerText=operators[i];
+        container.appendChild(btn)
+    }
+    container.style.display = "grid";
+    container.style.gridTemplateColumns=`repeat(1), 2rem)`;
+
+}
+
+function buildOutputScreen() {
+    let container = document.querySelector("#outputContainer");
+    for (i=0;i<2;i++) {
+        let div = document.createElement("div");
+        div.setAttribute("id",`output${i}`);
+        div.setAttribute("class","outputText");
+        div.innerText=`test${i}`
+        container.appendChild(div)
+    }
+
+}
+
+function inputListener() {
+    let buttons = document.querySelectorAll(".calculatorButtons");
+    buttons.forEach(btn => {
+        btn.addEventListener("click", () => {
+
+        } )
+    })
+
+}
+
+buildCalculator()
